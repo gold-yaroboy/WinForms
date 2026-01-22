2e158b09009fc04a0ad3db799b6f7546075cb224
ilyav <ilyav@ILIA>
2026-01-22 23:52:13 +0500

    Settings DONE

--- Clock/MainForm.cs
+++ Clock/MainForm.cs
@@ -64,7 +64,7 @@ namespace Clock
 			writer.WriteLine(labelTime.ForeColor.ToArgb());
 
 			writer.WriteLine(fontDialog.Filename);
-			//writer.WriteLine(labelTime.Font.OriginalFontName);
+			writer.WriteLine(labelTime.Font.Size);
 
 			writer.Close();
 
@@ -94,7 +94,7 @@ namespace Clock
 				labelTime.BackColor = backgroundColorDialog.Color = Color.FromArgb(Convert.ToInt32(reader.ReadLine()));
 				labelTime.ForeColor = foregroundColorDialog.Color = Color.FromArgb(Convert.ToInt32(reader.ReadLine()));
 
-				fontDialog = new FontDialog(reader.ReadLine());
+				fontDialog = new FontDialog(reader.ReadLine(), reader.ReadLine());
 				labelTime.Font = fontDialog.Font;
 
 				reader.Close();

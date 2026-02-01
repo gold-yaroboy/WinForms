188e1b520b247206b4896e91466895e7f9255075
ilyav <ilyav@ILIA>
2026-02-01 15:14:28 +0500

    Setup 50\50

---
+++ Autorunner/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Autoranner
+{
+	class Programm
+	{
+		static void Main(string[] args)
+		{
+			Process.Start("Clock.exe");
+		}
+	}
+}
